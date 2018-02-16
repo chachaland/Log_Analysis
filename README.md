97aa54765b40d968be2606f3941c45faa2f89d29
Jeesoo Lee <ijsoo99@hotmail.com>
2018-02-16 03:44:29 -0500

    Files edited for resubmission
    log_analysis.py: improved queries and included correct shebang line
    README: overall improvement
    results: new results

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Log Analysis
 
-This project is part of Udacity's Full Stack Web Developer Nanodegree. It is to create a reporting tool that prints out reports in plain text based on the data in the database.
+Part of Udacity's Full Stack Web Developer Nanodegree
+
+This project sets up a mock PostgreSQL database for a fictional news website. The provided Python script uses the psycopg2 library to query the database and produce a report that answers the following three questions:
+
+* What are the most popular three articles of all time?
+* Who are the most popular article authors of all time?
+* On which days did more than 1% of requests lead to errors?
 
 ### How to Run?
 
@@ -10,13 +16,18 @@ This project is part of Udacity's Full Stack Web Developer Nanodegree. It is to 
 * VirtualBox
 
 #### Setup
-1. Clone or download [fullstack-nanodegree-vm](https://github.com/udacity/fullstack-nanodegree-vm) repository
-2. Download the [newsdata.zip](https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip)
-3. Copy the newsdata.sql file from the zip file you downloaded into the Vagrant sub-directory in the fullstack-nanodegree-vm repository
-4. Clone this repository into Vagrant sub-directory as well
+1. Clone this repository
+2. Download and save `VagrantFile` in the repository by running the following command:
+```
+curl https://raw.githubusercontent.com/udacity/fullstack-nanodegree-vm/master/vagrant/Vagrantfile
+```
+*This file is required to launch Vagrant VM*
+3. Download and unzip the [newsdata.zip](https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip)
+4. Copy the `newsdata.sql` file from the zip file you downloaded into the repository
+
 
 #### To Run
-1. Launch Vagrant VM by running `vagrant up` command in Vagrant sub-directory
+1. Launch Vagrant VM by running `vagrant up` command
 2. Log in with `vagrant ssh` command
 3. Use the command `psql -d news -f newsdata.sql` to connect a database called `news` in your Vagrant VM
 
--- log_analysis.py
+++ log_analysis.py
@@ -1,32 +1,28 @@
-
+#!/usr/bin/env python3
 
 import psycopg2
 
 
 DBNAME = "news"
 
-# run_query: running a query with psql using psycopg2
-
 
 def run_query(query):
+    """run_query: running a query with psql using psycopg2"""
     db = psycopg2.connect(database=DBNAME)
     cursor = db.cursor()
     cursor.execute(query)
     results = cursor.fetchall()
-    return results
     db.close()
-
-# result_text: prints questions and results in plain text
+    return results
 
 
 def result_text(question, results, col2):
+    """result_text: prints questions and results in plain text"""
     print('')
     print(question)
     print('')
-    ii = 1
-    for i in results:
+    for ii, i in enumerate(results, 1):
         print('     {})  {} ---- {} {}'.format(ii, i[0], i[1], col2))
-        ii += 1
     print('')
 
 # List of questions + queries
@@ -35,43 +31,51 @@ def result_text(question, results, col2):
 question_1 = "1. What are the most popular three articles of all time? "
 
 query_1 = '''
-        select distinct articles.title, three_top.views from
-        (select regexp_replace(log.path, '/article/', '', 'g') as slug,
-            count(log.path) as views from log
-            group by path
-            order by views desc
-            limit 3 offset 1) as three_top
-        left join articles
-        on three_top.slug = articles.slug
-        order by views desc;
+        SELECT distinct articles.title, total_views.views
+        FROM (
+            SELECT log.path, count(*) AS views
+            FROM log
+            GROUP BY path
+        ) AS total_views
+        INNER JOIN articles
+        ON total_views.path = '/article/' || articles.slug
+        ORDER BY views DESC
+        LIMIT 3;
 '''
 question_2 = "2. Who are the most popular article authors of all time?"
 
 query_2 = '''
-        select authors_articles.name,
-        sum(total_views.views) as views from
-            (select regexp_replace(log.path, '/article/', '', 'g') as slug,
-                count(log.path) as views from log
-                group by path) as total_views,
-            (select distinct articles.slug, authors.name
-                from articles join authors
-                on authors.id = articles.author) as authors_articles
-        where authors_articles.slug = total_views.slug
-        group by name
-        order by views desc
-        limit 3;
+        SELECT authors_articles.name,
+        SUM(total_views.views) AS views
+        FROM (
+            SELECT regexp_replace(log.path, '/article/', '', 'g') AS slug,
+            COUNT(log.path) AS views
+            FROM log
+            GROUP BY path
+        ) AS total_views,
+            (
+            SELECT distinct articles.slug, authors.name
+            FROM articles JOIN authors
+            ON authors.id = articles.author
+        ) AS authors_articles
+        WHERE authors_articles.slug = total_views.slug
+        GROUP BY name
+        ORDER BY views DESC
+        LIMIT 3;
 '''
 
 question_3 = "3. On which days did more than 1% of requests lead to errors?"
 
 query_3 = '''
-        select fail_table.MDY,fail_table.fail_per from
-        (select round(sum(case when status = '404 NOT FOUND' then 1 end)*100.0/
-        count(log.status),2) as fail_per,
-            to_char(log.time, 'MonthDD,YYYY') as MDY
-            from log
-            group by MDY) as fail_table
-        where fail_per >= 1
+        SELECT to_char(MDY, 'FMMonth DD, YYYY'), fail_table.fail_per
+        FROM (
+            SELECT round(SUM(CASE WHEN status = '404 NOT FOUND' THEN 1 END)
+            * 100.0/ COUNT(log.status),2) AS fail_per,
+            time::date AS MDY
+            FROM log
+            GROUP BY MDY
+        ) AS fail_table
+        WHERE fail_per >= 1
 '''
 
 # Main Function: Running queries and fetching the results
